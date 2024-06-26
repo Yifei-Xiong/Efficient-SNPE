e99edeaa0672a2713c587d3df6443b1a1c11c69f
Night Wso <hzyzyts@126.com>
2024-06-26 15:44:36 +0800

    Create task_example.sh

---
+++ task_example.sh
@@ -0,0 +1,25 @@
+python3 Inference.py --method SNPEB --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr a1 --data 1 --seed 10000;
+python3 Inference.py --method SNPEB --ess 0.5 --calib 0.1 --dr 0.2 --dt 1 --reuse 1 --gpu 1 --mkstr b1 --data 1 --seed 10000;
+python3 Inference.py --method APT --ess 0.0 --calib 0.0 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr c1 --data 1 --seed 10000;
+python3 Inference.py --method APT --ess 0.5 --calib 0.1 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr d1 --data 1 --seed 10000;
+
+python3 Inference.py --method SNPEB --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr a2 --data 2 --seed 10000;
+python3 Inference.py --method SNPEB --ess 0.5 --calib 0.1 --dr 0.2 --dt 1 --reuse 1 --gpu 1 --mkstr b2 --data 2 --seed 10000;
+python3 Inference.py --method APT --ess 0.0 --calib 0.0 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr c2 --data 2 --seed 10000;
+python3 Inference.py --method APT --ess 0.5 --calib 0.1 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr d2 --data 2 --seed 10000;
+
+python3 Inference.py --method SNPEB --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr a3 --data 3 --seed 10000;
+python3 Inference.py --method SNPEB --ess 0.5 --calib 0.1 --dr 0.2 --dt 0 --reuse 1 --gpu 1 --mkstr b3 --data 3 --seed 10000;
+python3 Inference.py --method APT --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr c3 --data 3 --seed 10000;
+python3 Inference.py --method APT --ess 0.5 --calib 0.1 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr d2 --data 2 --seed 10000;
+
+python3 Inference.py --method SNPEB --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr a4 --data 4 --seed 10000;
+python3 Inference.py --method SNPEB --ess 0.5 --calib 0.1 --dr 0.2 --dt 1 --reuse 1 --gpu 1 --mkstr b4 --data 4 --seed 10000;
+python3 Inference.py --method APT --ess 0.0 --calib 0.0 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr c4 --data 4 --seed 10000;
+python3 Inference.py --method APT --ess 0.5 --calib 0.1 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr d4 --data 4 --seed 10000;
+
+python3 Inference.py --method SNPEB --ess 0.0 --calib 0.0 --dr 0.0 --dt 0 --reuse 0 --gpu 1 --mkstr a5 --data 5 --seed 10000;
+python3 Inference.py --method SNPEB --ess 0.5 --calib 0.1 --dr 0.2 --dt 1 --reuse 1 --gpu 1 --mkstr b5 --data 5 --seed 10000;
+python3 Inference.py --method APT --ess 0.0 --calib 0.0 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr c5 --data 5 --seed 10000;
+python3 Inference.py --method APT --ess 0.5 --calib 0.1 --dr 0.0 --dt 1 --reuse 0 --gpu 1 --mkstr d5 --data 5 --seed 10000;
+
